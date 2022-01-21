e20187affd08bd6468692362365af866fbad19de
jcpcabanada <jcpcabanada@gmail.com>
2022-01-21 14:42:14 -0800

    4th

--- data/migrations/20220121221933_initial-migrations.js
+++ data/migrations/20220121221933_initial-migrations.js
@@ -47,5 +47,9 @@ exports.up = async function(knex) {
 };
 
 exports.down = async function(knex) {
-  
+    await knex.schema
+        .dropTableIfExists('project_resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects')
 };
